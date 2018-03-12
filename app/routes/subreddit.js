bf772e36f253fb30d135920283cfc23f67de9505
Jordan Bergero <bergero@sonoma.edu>
2018-03-12 10:19:58 -0700

    clicking on the name of a subreddit from anywhere now links to that subreddit within the readit app, instead of linking to reddit.com

--- app/routes/subreddit.js
+++ app/routes/subreddit.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model(params){
 		var subreddit = params.subreddit_id;
-		var url = "http://www.reddit.com/r/"+subreddit+"/.json";
+		var url = "http://www.reddit.com/"+subreddit+"/.json";
 		return Ember.$.getJSON(url);
 	}
 });
