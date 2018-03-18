c543d581f50be10d4b398c07ebde2e8c84a907be
Jordan Bergero <bergero@sonoma.edu>
2018-03-17 17:39:46 -0700

    Helpers are amazing. Fixed the display of a post's score to resemble reddit. posts over a 1000 points now display as the abbreviated form with a 'k' following it.

---
+++ app/helpers/format-score.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export function formatScore(params/*, hash*/) {
+	var score = params[0];
+	if (score/1000 >= 1) {
+		score = score/1000;
+		score = Math.round(score * 10) / 10;
+		score = score + "K";
+	}
+	else{
+		score = score;
+	}
+	return score;
+}
+
+export default Ember.Helper.helper(formatScore);
